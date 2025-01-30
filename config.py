5a6d7785ba3c2c6a0a0c4b1abf0a464cc7c348ce
CoderRzps <ayushpritysingh098@gmail.com>
2025-01-30 15:00:03 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,13 +5,14 @@ import logging
 
 # import dotenv
 # dotenv.load_dotenv()
-
+
+
 
 from logging.handlers import RotatingFileHandler
 
 #force user to join your backup channel leave 0 if you don't need.
-FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "-1002266025682"))
-FORCE_SUB_CHANNEL2 = int(os.environ.get("FORCE_SUB_CHANNEL2", "-1002485124074"))
+FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "-1002172623381"))
+FORCE_SUB_CHANNEL2 = int(os.environ.get("FORCE_SUB_CHANNEL2", "-1002193015686"))
 
 if FORCE_SUB_CHANNEL > FORCE_SUB_CHANNEL2:
     temp = FORCE_SUB_CHANNEL2 
