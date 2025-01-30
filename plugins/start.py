9c9de25f93e874dc09c09d19a743a355b2d385d2
CoderRzps <ayushpritysingh098@gmail.com>
2025-01-30 21:26:07 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -199,14 +199,14 @@ async def start_command(client: Client, message: Message):
                 link = await get_shortlink(SHORTLINK_API_URL, SHORTLINK_API_KEY, f'{newLink}')
                 if USE_PAYMENT:
                     btn = [
-                        [InlineKeyboardButton("Click Here 👆", url=link),
-                         InlineKeyboardButton('How to open this link 👆', url=TUT_VID)],
-                        [InlineKeyboardButton("Buy Premium plan", callback_data="buy_prem")]
+                        [InlineKeyboardButton("CLICK HERE 👆", url=link),
+                         InlineKeyboardButton('TUTORIAL', url=TUT_VID)],
+                        [InlineKeyboardButton("PREMIUM", callback_data="buy_prem")]
                     ]
                 else:
                     btn = [
-                        [InlineKeyboardButton("Click Here 👆", url=link)],
-                        [InlineKeyboardButton('How to open this link 👆', url=TUT_VID)]
+                        [InlineKeyboardButton("CLICK HERE 👆", url=link)],
+                        [InlineKeyboardButton('TUTORIAL', url=TUT_VID)]
                     ]
                 await message.reply(f"Total clicks {clicks}. Here is your link 👇.", reply_markup=InlineKeyboardMarkup(btn), protect_content=False, quote=True)
                 return
@@ -223,8 +223,8 @@ async def start_command(client: Client, message: Message):
         reply_markup = InlineKeyboardMarkup(
             [
                 [
-                    InlineKeyboardButton("😊 About Me", callback_data="about"),
-                    InlineKeyboardButton("🔒 Close", callback_data="close")
+                    InlineKeyboardButton("😊 ABOUT ME", callback_data="about"),
+                    InlineKeyboardButton("🔒 CLOSE", callback_data="close")
                 ]
             ]
         )
@@ -252,14 +252,14 @@ async def start_command(client: Client, message: Message):
             link = await get_shortlink(SHORTLINK_API_URL, SHORTLINK_API_KEY, f'https://telegram.dog/{client.username}?start=verify_{token}')
             if USE_PAYMENT:
                 btn = [
-                    [InlineKeyboardButton("Click Here 👆", url=link),
-                     InlineKeyboardButton('How to open this link 👆', url=TUT_VID)],
-                    [InlineKeyboardButton("Buy Premium plan", callback_data="buy_prem")]
+                    [InlineKeyboardButton("CLICK HERE 👆", url=link),
+                     InlineKeyboardButton('TUTORIAL', url=TUT_VID)],
+                    [InlineKeyboardButton("PREMIUM", callback_data="buy_prem")]
                 ]
             else:
                 btn = [
-                    [InlineKeyboardButton("Click Here 👆", url=link)],
-                    [InlineKeyboardButton('How to open this link 👆', url=TUT_VID)]
+                    [InlineKeyboardButton("CLICK HERE 👆", url=link)],
+                    [InlineKeyboardButton('TUTORIAL', url=TUT_VID)]
                 ]
             await message.reply(f"Your Ads token is expired, refresh your token and try again. \n\nToken Timeout: {get_exp_time(VERIFY_EXPIRE)}\n\nWhat is the token?\n\nThis is an ads token. If you pass 1 ad, you can use the bot for {get_exp_time(VERIFY_EXPIRE)} after passing the ad", reply_markup=InlineKeyboardMarkup(btn), protect_content=False, quote=True)
             return
@@ -332,7 +332,7 @@ async def gen_link_encoded(client: Bot, message: Message):
         await hash.reply("Cancelled 😉!")
         return
     link = f"https://t.me/{client.username}?start={hash.text}"
-    reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton("🎉 Click Here ", url=link)]])
+    reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton("🎉 CLICK HERE ", url=link)]])
     await hash.reply_text(f"<b>🧑‍💻 Here is your generated link", quote=True, reply_markup=reply_markup)
     return
         
